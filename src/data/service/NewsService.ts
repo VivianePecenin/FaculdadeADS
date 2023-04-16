72155cddcd274fd27accb524080824e6476f5fce
VivianePecenin <vivipece555@gmail.com>
2023-04-16 14:09:41 -0300

    Programação Mobile API de Receitas

--- src/data/service/NewsService.ts
+++ src/data/service/NewsService.ts
@@ -1,6 +1,6 @@
 import { ApiConfig } from "../../config/api";
 import { httpClient } from "../../infra/http";
-import { Categories, Headlines, Meals } from "../model";
+import { Categories, Headlines , Meals } from "../model";
 
 export const getCategory = async (): Promise<Categories[]> => {
     const response = await httpClient.get<Headlines>(ApiConfig.GET_TOP_CATEGORY)
